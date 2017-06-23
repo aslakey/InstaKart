a51069139fcb817740dd5356f279558b1883e54c
Austin Slakey <aslakey@mac.com>
2017-06-23 15:49:41 -0400

    Update data loading

--- Team1010_Instacart.py
+++ Team1010_Instacart.py
@@ -27,6 +27,7 @@ Where item_input will have the same dimensions as output, a reduced featurespace
 consisting of the most commonly reordered items.
 '''
 import time
+import os
 import numpy as np
 from matplotlib import pyplot as plt
 from keras.utils import np_utils
@@ -37,11 +38,40 @@ from keras.optimizers import RMSprop
 from keras import backend as K
 from sklearn.model_selection import train_test_split
 import pandas as pd
+from pandas.util.testing import assert_frame_equal
+
 
 _EPSILON = K.epsilon()
 _SEED = 7
 
-def load_data(filename, seed = 7):
+
+def load_validation():
+    '''load_validation
+    Simply returns the first set of trianing files as validation data
+    '''
+    test_file = '14'
+    cwd = os.getcwd()
+
+    test_file = '14'
+    folder = "/processed_training_files/"
+
+    test_Y = pd.read_csv(cwd+folder+'real_train_file_'+test_file+'.csv')
+    test_Number = pd.read_csv(cwd+folder+'ni_train_file_'+test_file+'.txt')
+    test_Item = pd.read_csv(cwd+folder+'pct_train_file_'+test_file+'.csv')
+    test_Department = pd.read_csv(cwd+folder+'dept_train_file_'+test_file+'.csv')
+
+    test_Y = test_Y.sort_values(by= ['user_id'])
+    test_Number = test_Number.sort_values(by= ['user_id'])
+    test_Item = test_Item.sort_values(by= ['user_id'])
+    test_Department = test_Department.sort_values(by= ['user_id'])
+
+    assert_frame_equal(test_Y[['user_id']].reset_index(drop=True), test_Number[['user_id']].reset_index(drop=True))
+    assert_frame_equal(test_Y[['user_id']].reset_index(drop=True), test_Item[['user_id']].reset_index(drop=True))
+    assert_frame_equal(test_Y[['user_id']].reset_index(drop=True), test_Department[['user_id']].reset_index(drop=True))
+
+    return(test_Y, test_Number, test_Item, test_Department)
+
+def load_batches(epoch):
     '''
     Expects data to be in a certain format (to be determined) Does a 20% split using seed
     as RandomState.
@@ -51,22 +81,33 @@ def load_data(filename, seed = 7):
         seed (int) - seed to use to split test and train
 
     Returns: 
-        test_Y, test_Number, test_Item, test_Department, train_Y, [train_Number, train_Item, train_Department - 
+        test_Y, test_Number, test_Item, test_Department - 
             as numpy matrices
     Raises:
         AssertionError - if test_Y, train_Y, train_Item, test_Item do not all have the same shape
     '''
-    data = pd.read_csv(filename, error_bad_lines=False,header=None)
-    train, test = train_test_split(data, test_size = 0.2, random_state = seed)
+    if epoch < 1 or epoch > 13:
+            raise ValueError("Epoch can only be between 1 and 13")
 
-    #test_Y, test_X = test[0].as_matrix(), test.ix[:, test.columns != 0].as_matrix()
-    #train_Y, train_X = train[0].as_matrix(), train.ix[:, train.columns != 0].as_matrix()
+    cwd = os.getcwd()
 
-    assert test_Y.shape == test_Item.shape
-    assert test_Y.shape == train_Y.shape
-    assert test_Y.shape == train_Item.shape
-    
-    return (test_Y, test_Number, test_Item, test_Department, train_Y, train_Number, train_Item, train_Department)
+    test_file = str(epoch)
+
+    train_Y = pd.read_csv(cwd+folder+'real_train_file_'+test_file+'.csv')
+    train_Number = pd.read_csv(cwd+folder+'ni_train_file_'+test_file+'.txt')
+    train_Item = pd.read_csv(cwd+folder+'pct_train_file_'+test_file+'.csv')
+    train_Department = pd.read_csv(cwd+folder+'dept_train_file_'+test_file+'.csv')
+
+    train_Y = train_Y.sort_values(by= ['user_id'])
+    train_Number = train_Number.sort_values(by= ['user_id'])
+    train_Item = train_Item.sort_values(by= ['user_id'])
+    train_Department = train_Department.sort_values(by= ['user_id'])
+
+    assert_frame_equal(train_Y[['user_id']].reset_index(drop=True), train_Number[['user_id']].reset_index(drop=True))
+    assert_frame_equal(train_Y[['user_id']].reset_index(drop=True), train_Item[['user_id']].reset_index(drop=True))
+    assert_frame_equal(train_Y[['user_id']].reset_index(drop=True), train_Department[['user_id']].reset_index(drop=True))
+
+    return(train_Y, train_Number, train_Item, train_Department)
 
 class LossHistory(cb.Callback):
     '''
@@ -80,13 +121,8 @@ class LossHistory(cb.Callback):
         batch_loss = logs.get('loss')
         self.losses.append(batch_loss)
 
-def _sigmoid_cross_entropy(y_true, y_pred):
 
-    y_pred = K.clip(y_pred, _EPSILON, 1.0-_EPSILON)
-    out = -(y_true * K.log(y_pred) + (1.0 - y_true) * K.log(1.0 - y_pred))
-    return K.mean(out, axis=-1)
-
-def init_model(numer_dim, item_dim, dept_dim):
+def init_model(numer_dim = 12, item_dim = 20000, dept_dim = 23):
     '''
     Creates model Graph and compiles it
     '''
@@ -115,7 +151,7 @@ def init_model(numer_dim, item_dim, dept_dim):
 
     item_dept_model = Sequential()
     item_dept_model.add(merged1)
-    item_dept_model.add(Dense(8000, input_dim=item_dim))
+    item_dept_model.add(Dense(12000, input_dim=item_dim))
     item_dept_model.add(Activation('relu'))
     item_dept_model.add(Dropout(0.4))
 
@@ -127,24 +163,24 @@ def init_model(numer_dim, item_dim, dept_dim):
     final_model.add(Activation('relu'))
     final_model.add(Dropout(0.4))
     final_model.add(Dense(item_dim))
-    #final_model.add(Activation('sigmoid'))
+    final_model.add(Activation('sigmoid'))
 
     #recommended param values, but we can test a little here (especially lr)
     rms = RMSprop(lr=0.01, epsilon=1e-08, decay=0.0)
 
     #compile Graph
-    model.compile(loss=_sigmoid_cross_entropy, optimizer=rms, metrics=['accuracy'])
+    model.compile(loss='binary_crossentropy', optimizer=rms, metrics=['accuracy'])
     print ('Model compield in {0} seconds'.format(time.time() - start_time))
     return model
 
-def run_network(data=None, model=None, epochs=20, batch=200):
+def run_network(valid_data=None, model=None, epochs=13, batch=32):
     try:
         start_time = time.time()
-        if data is None:
+        if valid_data is None:
             filename = "FILENAME HERE"
-            test_Y, test_Number, test_Item, test_Department, train_Y, train_Number, train_Item, train_Department = load_data(filename)
+            test_Y, test_Number, test_Item, test_Department = load_validation(filename)
         else:
-            test_Y, test_Number, test_Item, test_Department, train_Y, train_Number, train_Item, train_Department = data
+            test_Y, test_Number, test_Item, test_Department = valid_data
 
         if model is None:
             model = init_model()
@@ -158,12 +194,26 @@ def run_network(data=None, model=None, epochs=20, batch=200):
         callbacks_list = [checkpoint] + history
 
         print ('Training model...')
-        model.fit([train_Number, train_Item, train_Department], train_Y, 
-                    epochs=epochs, 
-                    batch_size=batch,
-                    callbacks=callbacks_list,
-                    validation_data=([test_Number, test_Item, test_Department], test_Y), 
-                    verbose=2)
+        #More code to use gernerators: https://keras.io/models/sequential/ and https://github.com/fchollet/keras/issues/68
+        #one epoch will be one file. 1000 users, 131 files
+        for e in range(epochs):
+            print("epoch %d" % e)
+            for train_Y, train_Number, train_Item, train_Department in load_batches(e): # these are chunks of ~1k users
+                model.fit([train_Number, train_Item, train_Department], train_Y, 
+                            epochs=1, 
+                            batch_size=batch,
+                            callbacks=callbacks_list,
+                            validation_data=([test_Number, test_Item, test_Department], test_Y), 
+                            verbose=2)
+                '''
+        model.fit_generator(generator(file_path = path),
+                            steps_per_epoch = samples_per_epoch//batch,
+                            epochs=epochs, 
+                            batch_size=batch,
+                            callbacks=callbacks_list,
+                            validation_data=([test_Number, test_Item, test_Department], test_Y), 
+                            verbose=2)
+                '''
 
         print ("Training duration : {0}".format(time.time() - start_time))
         score = model.evaluate([test_Number, test_Item, test_Department], test_Y, batch_size=16)
@@ -181,3 +231,11 @@ def plot_losses(losses):
     ax.plot(losses)
     ax.set_title('Loss per batch')
     fig.show()
+
+
+
+
+
+
+
+
